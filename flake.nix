ca7105edfb41c56c4ab4173b09cb2056a819475d
Mike Kusold <git@mikekusold.com>
2024-12-17 12:20:07 -0700

    Update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Granite Flakes that make up the RockyMTN";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
   };
 
   outputs = {

63ef5682d96b5a59481fbc3e19a04ff622340ce5
Mike Kusold <git@mikekusold.com>
2026-01-18 15:29:52 -0700

    Switch to flake-parts and support linux systems

--- flake.nix
+++ flake.nix
@@ -4,19 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       nixpkgs-unstable,
       home-manager,
+      flake-parts,
       ...
-    }@inputs:
-    let
-      inherit (self) outputs;
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.home-manager.flakeModules.home-manager
+      ];
+
       systems = [
         "aarch64-linux"
         "i686-linux"
@@ -25,45 +33,66 @@
         "x86_64-darwin"
       ];
 
-      # This is a function that generates an attribute by calling a function you
-      # pass to it, with each system as an argument
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      formatter = forAllSystems (system: nixpkgs-unstable.legacyPackages.${system}.nixfmt-tree);
+      perSystem =
+        { system, ... }:
+        {
+          formatter = nixpkgs-unstable.legacyPackages.${system}.nixfmt-tree;
+        };
 
-      homeConfigurations = {
-        "mike" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          extraSpecialArgs = {
-            pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin;
-          };
-          modules = [
-            self.homeConfigurationModules.mike
-            {
-              home.username = "mike";
-              home.homeDirectory = "/Users/mike";
-            }
-          ];
+      flake = {
+        # Reusable home-manager modules
+        homeModules = {
+          mike = ./home-manager/mike/home.nix;
+          mkusold = ./home-manager/mkusold/home.nix;
         };
-        "mkusold" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          extraSpecialArgs = {
-            pkgs-unstable = nixpkgs-unstable.legacyPackages.aarch64-darwin;
+
+        homeConfigurations =
+          let
+            mkHome =
+              {
+                username,
+                system,
+                homeDirectory ?
+                  if builtins.match ".*-darwin" system != null then "/Users/${username}" else "/home/${username}",
+              }:
+              home-manager.lib.homeManagerConfiguration {
+                pkgs = import nixpkgs { inherit system; };
+                extraSpecialArgs = {
+                  inherit inputs system;
+                  pkgs-unstable = import nixpkgs-unstable { inherit system; };
+                };
+                modules = [
+                  self.homeModules.${username}
+                  {
+                    home.username = username;
+                    home.homeDirectory = homeDirectory;
+                  }
+                ];
+              };
+          in
+          {
+            # mike's configurations (hostname-based for automatic detection)
+            "mike@Mac.int.rockymtn.org" = mkHome {
+              username = "mike";
+              system = "aarch64-darwin";
+            };
+
+            # mkusold's configurations
+            "mkusold@Mikes-MacBook-Pro.local" = mkHome {
+              username = "mkusold";
+              system = "aarch64-darwin";
+            };
+
+            # Fallback configurations (when hostname doesn't match)
+            "mike" = mkHome {
+              username = "mike";
+              system = "x86_64-linux";
+            };
+            "mkusold" = mkHome {
+              username = "mkusold";
+              system = "x86_64-linux";
+            };
           };
-          modules = [
-            self.homeConfigurationModules.mkusold
-            {
-              home.username = "mkusold";
-              home.homeDirectory = "/Users/mkusold";
-            }
-          ];
-        };
-      };
-      # Attempt to let the user config to be imported by nixos homemanager
-      homeConfigurationModules = {
-        "mike" = ./home-manager/mike/home.nix;
-        "mkusold" = ./home-manager/mkusold/home.nix;
       };
     };
 }

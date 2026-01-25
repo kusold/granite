4cd9092131ebac5cbb1b3d83f25fd5d67c40dfc4
Mike Kusold <git@mikekusold.com>
2026-01-24 18:54:08 -0700

    Add back zshenv

--- home-manager/_modules/zsh.nix
+++ home-manager/_modules/zsh.nix
@@ -12,5 +12,14 @@
     enable = true;
     # dotDir doesn't allow me to manage that directory myself
     #dotDir = ".config/zsh";
+    initContent = lib.mkBefore ''
+      export ZDOTDIR=$${ZDOTDIR:-~/.config/zsh}
+    #   export ZDOTDIR=~/.config/zsh
+    #   . $ZDOTDIR/.zshenv
+    #   . $ZDOTDIR/.zlogin
+    #   . $ZDOTDIR/.zprofile
+
+      . $ZDOTDIR/.zshrc
+    '';
   };
 }
--- nix/parts/home-configurations/mike.nix
+++ nix/parts/home-configurations/mike.nix
@@ -5,6 +5,13 @@
     "mike@Mac.int.rockymtn.org" = self.lib.mkHome {
       username = "mike";
       system = "aarch64-darwin";
+      hostname = "Mac.int.rockymtn.org";
+    };
+
+    "mike@dev" = self.lib.mkHome {
+      username = "mike";
+      system = "x86_64-linux";
+      hostname = "dev";
     };
 
     # Fallback configuration (when hostname doesn't match)
